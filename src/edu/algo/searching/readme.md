4051a776b714b0e3d9d5760b7732f8098d977c8b
Ravi Yadav <tech.raviyadav@gmail.com>
2020-04-07 12:20:59 +0530

    add package structure

---
+++ src/edu/algo/searching/readme.md
@@ -0,0 +1,2 @@
+###Searching Notes
+[Binary Search Condtion Improvement](https://www.geeksforgeeks.org/the-ubiquitous-binary-search-set-1/)
--- src/edu/datastructure/impl/Main.java
+++
@@ -1,8 +0,0 @@
-package edu.datastructure.impl;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
